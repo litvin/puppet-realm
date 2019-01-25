5645aa0858367603ce21b0107c9e1fe28d85efa8
litvin <litvin@mail.ru>
2019-01-25 06:49:33 +0300

    fix bugs

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class realm::params {
 	$domain_name  = 'am.local'	
 	$host_ad_name = 'dc-01.am.local'
-	$admin_passwd = 'Qwerty1!'
-	$admin	      = 'rc'
+	$admin_passwd = ''
+	$admin	      = ''
 	$package_name = ['realmd', 'sssd', 'oddjob', 'oddjob-mkhomedir', 'adcli', 'samba-common', 'samba-common-tools']
 }
